1b969820f4940719cb3aba59a142ec1dc34b7146
Mikhail Rantovich <mrantovich@gmail.com>
2021-06-23 15:35:25 +0700

    Меню сверху контента на мобильных

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
+const wholeBody = document.querySelector('body');
+const wholeHTML = document.querySelector('html');
 burger.addEventListener('click', toggleActive);
 
 function toggleActive() {
 	burger.classList.toggle('_active');
 	menu.classList.toggle('_active');
+	wholeBody.classList.toggle('_lock');
+	wholeHTML.classList.toggle('_lock');
 };
 
 const swiper = new Swiper('.servs__swiper', {
